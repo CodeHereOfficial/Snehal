b4b3c7ced8ac24d8d1502f949aea1608f817371d
CodeHereOfficial <snehal.rajapurkar@cognizant.com>
2022-07-22 12:28:57 +0530

    3 files modified

--- src/app/colorfull1/colorfull1.component.ts
+++ src/app/colorfull1/colorfull1.component.ts
@@ -3,10 +3,47 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'colorfull1',
   templateUrl: './colorfull1.component.html',
-  styleUrls: ['./colorfull1.component.css']
+  styleUrls: ['./colorfull1.component.css'],
 })
 export class Colorfull1Component {
-
   
-
+  public headers = ['Name', 'Hobby', 'Age', 'Country'];
+  public rows = [
+    {
+      Name: 'Rani',
+      Hobby: 'Dance',
+      Age: '21',
+      Country: 'India',
+    },
+    {
+      Name: 'Seema',
+      Hobby: 'Singing',
+      Age: '20',
+      Country: 'India',
+    },
+    {
+      Name: 'Prem',
+      Hobby: 'Memecry',
+      Age: '19',
+      Country: 'India',
+    },
+    {
+      Name: 'Sam',
+      Hobby: 'Dance',
+      Age: '21',
+      Country: 'Australia',
+    },
+    {
+      Name: 'Rani',
+      Hobby: 'Dance',
+      Age: '21',
+      Country: 'India',
+    },
+    {
+      Name: 'Rani',
+      Hobby: 'Dance',
+      Age: '21',
+      Country: 'India',
+    },
+  ];
 }
