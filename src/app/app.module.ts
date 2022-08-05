b4a81c137c5c30cd02cce9b6a775f89418214229
CodeHereOfficial <snehal.rajapurkar@cognizant.com>
2022-08-05 15:28:01 +0530

    3 files modified, 4 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
+
 import { Colorfull1Component } from './colorfull1/colorfull1.component';
 import { StatusTypeComponent } from './status-type/status-type.component';
+
 import { AlertdialogService } from './alertdialog.service';
+import { AppRoutingModule, routingComponent } from './app-routing.module';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  providers:  [AlertdialogService],
-  declarations: [ AppComponent, HelloComponent, Colorfull1Component, StatusTypeComponent ],
-  bootstrap:    [ AppComponent ]
+  imports: [BrowserModule, FormsModule, AppRoutingModule],
+  providers: [AlertdialogService],
+  declarations: [
+    AppComponent,
+    Colorfull1Component,
+    StatusTypeComponent,
+    routingComponent,
+  ],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
