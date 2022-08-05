1244e606b29da327aa9253f5078e762b05eca67f
CodeHereOfficial <snehal.rajapurkar@cognizant.com>
2022-08-05 15:58:02 +0530

    2 files modified

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -1,15 +1,27 @@
 import { Component, OnInit } from '@angular/core';
+// import productData from '../db.json';
+
+// interface Product {
+//   id: number;
+//   title: string;
+//   description: string;
+//   price: number;
+//   discountPercentage: number;
+//   rating: number;
+//   stock: number;
+//   brand: string;
+//   category: string;
+//   thumbnail: any;
+//   images: any;
+// }
 
 @Component({
   selector: 'app-product-list',
   templateUrl: './product-list.component.html',
-  styleUrls: ['./product-list.component.css']
+  styleUrls: ['./product-list.component.css'],
 })
 export class ProductListComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  constructor() {}
+  // product: Product[] = productData;
+  ngOnInit() {}
 }
